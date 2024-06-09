ecd2c50f2c04583c6cea07a19a104d2c2b7b2d3a
frankassafuah <frankassafuah@gmail.com>
2024-06-09 17:59:00 +0000

    building the cart page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -54,6 +54,8 @@ export const {
 
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, curr) => sum + curr.quantity, 0);
 
